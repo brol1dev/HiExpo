053fdc89a981f5a3b7e4f85d23aba06db618c0ed
brol1dev <eric.vargas.mx@gmail.com>
2024-03-10 19:40:50 -0500

    Add Drizzle SQLite support

--- App.tsx
+++ App.tsx
@@ -1,8 +1,32 @@
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { openDatabaseSync } from "expo-sqlite/next";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+
+import migrations from "./drizzle/migrations";
 import { MyBarChart } from "./app/charts";
 
+const expoDb = openDatabaseSync("db.db");
+const db = drizzle(expoDb);
+
 export default function App() {
+  const { success, error } = useMigrations(db, migrations);
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+  if (!success) {
+    return (
+      <View>
+        <Text>Migration is in progress...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working wara on your app!</Text>
